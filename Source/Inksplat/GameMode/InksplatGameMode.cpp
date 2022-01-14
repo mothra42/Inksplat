34e262a5e786805ad46aa612701b3a825e5eb776
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2022-01-13 16:53:40 -0800

    Setup initial testing arena

--- Source/Inksplat/GameMode/InksplatGameMode.cpp
+++ Source/Inksplat/GameMode/InksplatGameMode.cpp
@@ -9,9 +9,9 @@ AInksplatGameMode::AInksplatGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	//C:/Users/Battlestation One/Desktop/Active Projects/Inksplat/Content/FirstPersonCPP/Blueprints/FirstPersonCharacter.uasset
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/PlayerCharacter_BP"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
-
 	// use our custom HUD class
 	HUDClass = AInksplatHUD::StaticClass();
 }
