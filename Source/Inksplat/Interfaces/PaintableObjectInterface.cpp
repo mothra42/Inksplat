ab904ffaeff5f42189ee61e16a157f86c682b397
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2022-01-20 18:23:20 -0800

    Add Interface to hold common painting methods
    Adds interface to ensure all paintable actors and pawns will have a
    paintable object setup method that will help setup component and
    materials.

---
+++ Source/Inksplat/Interfaces/PaintableObjectInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PaintableObjectInterface.h"
+
+// Add default functionality here for any IPaintableObjectInterface functions that are not pure virtual.
