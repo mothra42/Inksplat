99a437d3d44dd2c976121618f19be4beb9a6a89e
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2022-02-17 18:54:37 -0800

    Adds first version of paint coverage calculation
    Adds a method and properties to calculate paint coverage of a skeletal
    mesh. Right now the method is terribly performant, but I hope to
    continue refining it and optimizing it to make it scale to more players.

--- Source/Inksplat/PaintableActorComponents/PaintableSkeletalMeshComponent.cpp
+++ Source/Inksplat/PaintableActorComponents/PaintableSkeletalMeshComponent.cpp
@@ -8,7 +8,10 @@
 #include "Kismet/GameplayStatics.h"
 #include "../CustomStaticMethods/SkeletalMeshPaintingLibrary.h"
 #include "Kismet/KismetRenderingLibrary.h"
+#include "Net/UnrealNetwork.h"
 #include "Engine/Canvas.h"
+#include "Engine/Engine.h"
+#include "Containers/Queue.h"
 
 UPaintableSkeletalMeshComponent::UPaintableSkeletalMeshComponent()
 {
@@ -24,7 +27,7 @@ UPaintableSkeletalMeshComponent::UPaintableSkeletalMeshComponent()
 void UPaintableSkeletalMeshComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
+	PaintCoverageArray.Init(0, 1048576);
 	FLinearColor ClearColor = FLinearColor(0.0, 0.0, 0.0, 0.0);
 	PaintTexture = UKismetRenderingLibrary::CreateRenderTarget2D(
 		GetWorld(),
@@ -51,20 +54,25 @@ void UPaintableSkeletalMeshComponent::GetLifetimeReplicatedProps(TArray<FLifetim
 
 bool UPaintableSkeletalMeshComponent::PaintMesh(const FHitResult& Hit, const FLinearColor& Color)
 {
+
 	UCanvas* Canvas;
 	FVector2D CanvasSize;
 	FDrawToRenderTargetContext Context;
 
 	FVector2D UVPosition;
-	//UGameplayStatics::FindCollisionUV(Hit, 0, UVPosition);
 	USkeletalMeshPaintingLibrary::FindCollisionUVFromHit(Hit, UVPosition);
-
 
 	UKismetRenderingLibrary::BeginDrawCanvasToRenderTarget(this, PaintTexture, Canvas, CanvasSize, Context);
 
 	//TODO make this work for textures as well. Will probably need to include one in the constructor.
 	//probably refactor this to exist in its own function
 	FVector2D TileItemPosition = CanvasSize * UVPosition;
+	if (GetOwner()->GetLocalRole() == ROLE_Authority)
+	{
+		//HealthCheckQueue.Enqueue(FHealthCheckInstructions(TileItemPosition, 25, 25));
+		//ConsumeHealthCheckQueue();
+		CalculateHealth(TileItemPosition, 25, 25);
+	}
 	FCanvasTileItem RectItem(
 		TileItemPosition,
 		FVector2D(25.f, 25.f), //size
@@ -75,3 +83,58 @@ bool UPaintableSkeletalMeshComponent::PaintMesh(const FHitResult& Hit, const FLi
 	UKismetRenderingLibrary::EndDrawCanvasToRenderTarget(this, Context);
 	return true;
 }
+
+void UPaintableSkeletalMeshComponent::ConsumeHealthCheckQueue()
+{
+	if (!bIsConsumingHealthCheck)
+	{
+		bIsConsumingHealthCheck = true;
+		while (!HealthCheckQueue.IsEmpty())
+		{
+			FHealthCheckInstructions CurrentInstructions;
+			if (HealthCheckQueue.Dequeue(CurrentInstructions))
+			{
+				CalculateHealth(CurrentInstructions.Origin, CurrentInstructions.UBounds, CurrentInstructions.VBounds);
+			}
+		}
+
+		bIsConsumingHealthCheck = false;
+	}
+}
+
+//TODO Consider adding a fourth argument that is a reference to a reference texture to check for the bounds of the UV map
+float UPaintableSkeletalMeshComponent::CalculateHealth(FVector2D Origin, int32 U_Bounds, int32 V_Bounds)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Starting calculate"));
+	int U = Origin.X - (U_Bounds/2);
+	int UEnd = Origin.X + (U_Bounds / 2);
+	int V = Origin.Y - (V_Bounds/2);
+	int VEnd = Origin.Y + (V_Bounds / 2);
+	int32 NewColoredPixelCount = 0;
+	while (U <= UEnd)
+	{
+		while (V <= VEnd)
+		{
+			if (U < 0 || V < 0 || U > 1024 || V > 1024)
+			{
+				V++;
+				continue;
+			}
+			else if (PaintCoverageArray[U + V * 1024] == 0)
+			{
+				NewColoredPixelCount++;
+				UE_LOG(LogTemp, Warning, TEXT("NewColor Pixel Count is %i"), NewColoredPixelCount);
+				PaintCoverageArray[U + V * 1024] = 1;
+			}
+
+			V++;
+		}
+		V = Origin.Y - (V_Bounds / 2);
+		U++;
+	}
+
+	//UE_LOG(LogTemp, Warning, TEXT("New colored pixels should be around 625 and is %i"), NewColoredPixelCount);
+	UE_LOG(LogTemp, Warning, TEXT("Finishing Calculate"));
+	return 0.0f;
+}
+
--- Source/Inksplat/PaintableActorComponents/PaintableSkeletalMeshComponent.h
+++ Source/Inksplat/PaintableActorComponents/PaintableSkeletalMeshComponent.h
@@ -6,6 +6,23 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "PaintableSkeletalMeshComponent.generated.h"
 
+USTRUCT()
+struct FHealthCheckInstructions
+{
+	GENERATED_BODY()
+
+	FVector2D Origin;
+	int32 UBounds;
+	int32 VBounds;
+
+	FHealthCheckInstructions() {}
+	FHealthCheckInstructions(
+		FVector2D InOrigin,
+		int32 InUBounds,
+		int32 InVBounds)
+		:Origin(InOrigin), UBounds(InUBounds), VBounds(InVBounds) {}
+};
+
 /**
  * 
  */
@@ -29,8 +46,20 @@ protected:
 
 	class UMaterialInstanceDynamic* MeshMaterialInstance;
 
+private:
+	TArray<int> PaintCoverageArray;
+
+	TQueue<FHealthCheckInstructions> HealthCheckQueue;
+
+	bool bIsConsumingHealthCheck = false;
+
 protected:
 	virtual void BeginPlay() override;
 
 	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+private:
+	void ConsumeHealthCheckQueue();
+
+	float CalculateHealth(FVector2D Origin, int32 U_Bounds, int32 V_Bounds);
 };
