b2e588b02b289ed4ce0a75fbfb1cd8be9f181e57
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2022-08-09 17:06:32 -0700

    Add logic for finding players in range when scanning

--- Source/Inksplat/AbilityComponents/PlayerScanAbilityComponent.cpp
+++ Source/Inksplat/AbilityComponents/PlayerScanAbilityComponent.cpp
@@ -1,5 +1,51 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AbilityComponents/PlayerScanAbilityComponent.h"
+#include "PlayerScanAbilityComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "../PlayerActor/PlayerCharacter.h"
+
+UPlayerScanAbilityComponent::UPlayerScanAbilityComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	SetIsReplicated(true);
+}
+
+void UPlayerScanAbilityComponent::ServerExecuteAbility_Implementation()
+{
+	//Find players in server
+	TArray<APlayerCharacter*> PlayersWithinRange = GetPlayerPawnsWithinRadius();
+
+	//Scan those players
+	RevealPlayers(PlayersWithinRange);
+}
+
+TArray<APlayerCharacter*> UPlayerScanAbilityComponent::GetPlayerPawnsWithinRadius()
+{
+	TArray<APlayerCharacter*> PlayersWithinRange{};
+	if (GetOwner()->GetLocalRole() == ROLE_Authority)
+	{
+		for (TObjectIterator<APlayerCharacter> Itr; Itr; ++Itr)
+		{
+			float DistanceBetweenPlayers = FVector::Distance(
+				GetOwner()->GetActorLocation(), 
+				Itr->GetActorLocation()
+			);
+			if (DistanceBetweenPlayers <= ScanRadius)
+			{
+				PlayersWithinRange.Add(*Itr);
+			}
+		}
+	}
+
+	return PlayersWithinRange;
+}
+
+void UPlayerScanAbilityComponent::RevealPlayers_Implementation(const TArray<APlayerCharacter*>& PlayersToReveal)
+{
+	//TODO
+	//change texture on skeletal mesh to reveal the outline.
+	//Design question to answer, do I show a snapshot? or does the effect last as long as the player uses the ability.
+	//I'm leaning towards the snapshot method for now, but I will make both to as I think its something that needs to be tested in playtesting.
+}
 
--- Source/Inksplat/AbilityComponents/PlayerScanAbilityComponent.h
+++ Source/Inksplat/AbilityComponents/PlayerScanAbilityComponent.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AbilityComponents/BaseAbilityComponent.h"
+#include "BaseAbilityComponent.h"
 #include "PlayerScanAbilityComponent.generated.h"
 
 /**
@@ -13,5 +13,19 @@ UCLASS()
 class INKSPLAT_API UPlayerScanAbilityComponent : public UBaseAbilityComponent
 {
 	GENERATED_BODY()
+public:
+	UPlayerScanAbilityComponent();
+
+protected:
+	//Scan radius is set to a default of 60 meters
+	UPROPERTY(Category = "Scan Properties", EditDefaultsOnly)
+	float ScanRadius = 6000.0f;
+
+private:
+	virtual void ServerExecuteAbility_Implementation() override;
 	
+	TArray<class APlayerCharacter*> GetPlayerPawnsWithinRadius();
+
+	UFUNCTION(Client, Reliable)
+	void RevealPlayers(const TArray<APlayerCharacter*>& PlayersToReveal);
 };
