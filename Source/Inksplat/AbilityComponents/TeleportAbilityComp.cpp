1496fcf80f0466b3106f540afb7a1a420a675331
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2022-08-12 15:09:28 -0700

    Add teleportation logic
    Adds some logic for finding teleportation location.

--- Source/Inksplat/AbilityComponents/TeleportAbilityComp.cpp
+++ Source/Inksplat/AbilityComponents/TeleportAbilityComp.cpp
@@ -1,5 +1,41 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AbilityComponents/TeleportAbilityComp.h"
+#include "TeleportAbilityComp.h"
+#include "Net/UnrealNetwork.h"
 
+UTeleportAbilityComp::UTeleportAbilityComp()
+{
+	SetIsReplicated(true);
+}
+
+void UTeleportAbilityComp::ServerExecuteAbility_Implementation()
+{
+	TeleportToLocation(FindTeleportLocation());
+}
+
+FVector UTeleportAbilityComp::FindTeleportLocation()
+{
+	FVector TeleportLocation;
+	//Using Forward vector is slightly fraut because it could be pointing up or down.
+	TeleportLocation = GetOwner()->GetActorLocation() + 
+		(GetOwner()->GetActorForwardVector() * TeleportRange);
+	return TeleportLocation;
+}
+
+bool UTeleportAbilityComp::IsLocationValid(const FVector LocationToTest)
+{
+	return false;
+	//TODO
+	//Check if location overlaps with existing level geometry, players are ok.
+	//check if location is within the valid bounds of the level. 
+}
+
+void UTeleportAbilityComp::TeleportToLocation(const FVector LocationToTeleportTo)
+{
+	GetOwner()->TeleportTo(LocationToTeleportTo,
+		GetOwner()->GetActorRotation(), 
+		false, 
+		true
+	);
+}
--- Source/Inksplat/AbilityComponents/TeleportAbilityComp.h
+++ Source/Inksplat/AbilityComponents/TeleportAbilityComp.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AbilityComponents/BaseAbilityComponent.h"
+#include "BaseAbilityComponent.h"
 #include "TeleportAbilityComp.generated.h"
 
 /**
@@ -14,4 +14,19 @@ class INKSPLAT_API UTeleportAbilityComp : public UBaseAbilityComponent
 {
 	GENERATED_BODY()
 	
+public:
+	UTeleportAbilityComp();
+
+protected:
+	UPROPERTY(Category = "Teleport Settings", EditDefaultsOnly)
+	float TeleportRange = 1000.f;
+
+private:
+	virtual void ServerExecuteAbility_Implementation() override;
+
+	FVector FindTeleportLocation();
+
+	bool IsLocationValid(const FVector LocationToTest);
+
+	void TeleportToLocation(const FVector LocationToTeleportTo);
 };
