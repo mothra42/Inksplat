6f56172478a4b0b589ade131c0a851d9019ca305
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2022-08-12 15:08:47 -0700

    Add new ability class
    This a teleport class intended for the Hunter role. This teleport ends with a splash of color around the location, potentially damaging other players near the location

---
+++ Source/Inksplat/AbilityComponents/TeleportAbilityComp.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilityComponents/BaseAbilityComponent.h"
+#include "TeleportAbilityComp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INKSPLAT_API UTeleportAbilityComp : public UBaseAbilityComponent
+{
+	GENERATED_BODY()
+	
+};
